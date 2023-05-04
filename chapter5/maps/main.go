84b4ac71b8bdd97a0e3c94c6c8512b59ea10b613
Johan <johan@jeksn.me>
2023-05-04 09:13:44 +0200

    map ok checks

--- chapter5/maps/main.go
+++ chapter5/maps/main.go
@@ -10,8 +10,32 @@ import "fmt"
 // }
 
 // maps have to be initialized before they can be used.
+// func main() {
+// 	x := make(map[string]int)
+// 	x["key"] = 10
+// 	fmt.Println(x["key"])
+// }
+
+// example of map
+
 func main() {
-	x := make(map[string]int)
-	x["key"] = 10
-	fmt.Println(x["key"])
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Beryllium"
+	elements["B"] = "Boron"
+	elements["C"] = "Carbon"
+	elements["N"] = "Nitrogen"
+	elements["O"] = "Oxygen"
+	elements["F"] = "Fluorine"
+	elements["Ne"] = "Neon"
+	//fmt.Println(elements["Li"])
+	// check for zero value
+	// name, ok := elements["Li"]
+	// fmt.Println(name, ok)
+	// if ok
+	if name, ok := elements["Li"]; ok {
+		fmt.Println(name, ok)
+	}
 }
