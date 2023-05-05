9e481926a6c6083ced51b59c460d86e2820eb101
Johan <johan@jeksn.me>
2023-05-05 16:12:10 +0200

    shorter way to define map

--- chapter5/maps/main.go
+++ chapter5/maps/main.go
@@ -17,24 +17,52 @@ import "fmt"
 // }
 
 // example of map
-
+//
+//	func main() {
+//		elements := make(map[string]string)
+//		elements["H"] = "Hydrogen"
+//		elements["He"] = "Helium"
+//		elements["Li"] = "Lithium"
+//		elements["Be"] = "Beryllium"
+//		elements["B"] = "Boron"
+//		elements["C"] = "Carbon"
+//		elements["N"] = "Nitrogen"
+//		elements["O"] = "Oxygen"
+//		elements["F"] = "Fluorine"
+//		elements["Ne"] = "Neon"
+//		//fmt.Println(elements["Li"])
+//		// check for zero value
+//		// name, ok := elements["Li"]
+//		// fmt.Println(name, ok)
+//		// if ok
+//		if name, ok := elements["Li"]; ok {
+//			fmt.Println(name, ok)
+//		}
+//	}
+//
+// shorter way to define map
 func main() {
-	elements := make(map[string]string)
-	elements["H"] = "Hydrogen"
-	elements["He"] = "Helium"
-	elements["Li"] = "Lithium"
-	elements["Be"] = "Beryllium"
-	elements["B"] = "Boron"
-	elements["C"] = "Carbon"
-	elements["N"] = "Nitrogen"
-	elements["O"] = "Oxygen"
-	elements["F"] = "Fluorine"
-	elements["Ne"] = "Neon"
-	//fmt.Println(elements["Li"])
-	// check for zero value
-	// name, ok := elements["Li"]
-	// fmt.Println(name, ok)
-	// if ok
+	elements := map[string]map[string]string{
+		"H": {
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"He": {
+			"name":  "Helium",
+			"state": "gas",
+		},
+		"Li": {
+			"name": "Lithium", "state": "solid",
+		},
+		"Be": {
+			"name":  "Beryllium",
+			"state": "solid",
+		},
+		"B": {"name": "Boron",
+			"state": "solid",
+		},
+		"C": {"name": "Carbon", "state": "solid"},
+	}
 	if name, ok := elements["Li"]; ok {
 		fmt.Println(name, ok)
 	}
