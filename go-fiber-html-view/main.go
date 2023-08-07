cd209207e6a9d9bd998a54921b9b2ebd2c243a3f
Johan <johan@jeksn.me>
2023-08-07 16:14:17 +0200

    testing go fiber

---
+++ go-fiber-html-view/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+	// Initialize standard Go html template engine
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	app.Get("/", func(c *fiber.Ctx) error {
+		// Render index template
+		return c.Render("index", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
