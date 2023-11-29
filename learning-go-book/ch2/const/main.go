3a6a25df7b42b6d56aa03645867b763bef3c85d0
Johan <johan@jeksn.me>
2023-11-29 11:02:04 +0100

    consts

---
+++ learning-go-book/ch2/const/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+const x = 10 
+const (
+idKey = "id"
+    nameKey = "name"
+)
+const z = 20 * 10 
+
+func main() {
+const y = "hello" 
+  fmt.Println(x)
+  fmt.Println(y) 
+
+  // Can't reassign const so an error is thrown
+  //  x=x+1
+  //  y = "bye"
+ 
+  fmt.Println(x)
+  fmt.Println(y)
+}
