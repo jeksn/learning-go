779c043e3e6e81b9c5bf36b5ec78e12970f176bb
Johan <johan@jeksn.me>
2023-12-01 20:58:46 +0100

    remove rand.seed and cleanup code

--- password-generator/main.go
+++ password-generator/main.go
@@ -3,24 +3,19 @@ package main
 import (
   "fmt"
   "math/rand"
-  "time"
 )
 
 const passwordLength = 15
 
-func main(){
-  rand.Seed(time.Now().UnixNano())
+func randomChar() rune {
+  chars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789%-_+#"
+  return rune(chars[rand.Intn(len(chars))])
+}
 
+func main(){
   var password string 
   for i := 0; i < passwordLength; i++ {
     password += string(randomChar())
   }
-
   fmt.Println(password)
-
-}
-
-func randomChar() rune {
-  chars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789%-_+#"
-  return rune(chars[rand.Intn(len(chars))])
 }
